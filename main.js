40c4a2173dcc03fb32dfb54fa05296cb566e398d
gihfranco21 <143224826+gihfranco21@users.noreply.github.com>
2024-09-27 08:58:33 -0300

    .

--- main.js
+++ main.js
@@ -4,4 +4,14 @@ function soma (num1, num2){
 }
 console.log(soma(5,3)); 
 
+area de um quadrado
 
+function areaQuadrado (lado){
+    return lado**2;
+
+}
+console.log(areaQuadrado(6));
+
+let media = function (1,2,3/3){
+    return 2;
+}
