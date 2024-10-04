ad749dc1ee1551f84d3b3c7528df4a6b2edc57e5
gihfranco21 <143224826+gihfranco21@users.noreply.github.com>
2024-10-04 08:32:09 -0300

    .

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
-function soma (num1, num2){
-    return num1 + num2;
+// function soma (num1, num2){
+//     return num1 + num2;
 
-}
-console.log(soma(5,3)); 
+// }
+// console.log(soma(5,3)); 
 
 
 
-function areaQuadrado (lado){
-    return lado**2;
+// function areaQuadrado (lado){
+//     return lado**2;
 
-}
-let valor1 = parseInt(prompt('qual valor?'));
-console.log(areaQuadrado(6));
+// }
+// let valor1 = parseInt(prompt('qual valor?'));
+// let valor2 = parseInt(prompt('qual valor?'));
+// let valor3 = parseInt(prompt('qual valor?'));
+// console.log(areaQuadrado(6));
 
-let media = function(x,y,z){
-    return (x+y+z)/3;
-}
-console.log(media(valor1,valor2,valor3));
+// let media = function(x,y,z){
+//     return (x+y+z)/3;
+// }
+// console.log(media(valor1,valor2,valor3));
+
+let estados=[
+    'Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba',
+    'Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
