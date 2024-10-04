84c2c82634d4fe1a543e2cc026d3c4d78180b8ee
gihfranco21 <143224826+gihfranco21@users.noreply.github.com>
2024-10-04 09:08:51 -0300

    df

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ let estados=[
     'Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
 
     let i=0;
-    while(i<27){
+    while(i<estados.length){
 console.log(i);
 console.log(estados[i]);
 i++;
